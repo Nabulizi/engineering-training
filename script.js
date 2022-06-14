6e8e310859ddf8da0dba0421747b6e95d12b0783
Nurali Abliz <nurali@Nuralis-MacBook-Pro.local>
2022-06-14 10:45:46 -0400

    DIG-71364 Add a function to toggle hidden class

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 console.log('Engineering Training!');
 
-const openButton=document.getElementById("modalButton");
-console.log("modalButton",openButton)
+const modalButton=document.getElementById("modalButton");
+// const modalContainer=document.getElementById("modal");
+modalButton.addEventListener("click",myFunction);
+let modalContainer = document.getElementById("modal");
+console.log("modalButton",modalButton)
+
+function myFunction() {
+    console.log("function is called!!!");
+    let modalContainer = document.getElementById("modal");
+    if (modalContainer.style.display === "block") {
+        modalContainer.style.display = "none";
+    } else {
+        modalContainer.style.display = "block";
+    }
+  }
