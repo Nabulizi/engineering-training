c9f1b9cfe92c415bddff0cfe0583221112ba0924
Nurali Abliz <nurali@Nuralis-MacBook-Pro.local>
2022-06-15 13:04:32 -0400

    DIG-71346 Add a function to toggle hidden class on close modal+optimization

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 console.log('Engineering Training!');
 
-const modalButton=document.getElementById("modalButton");
+const openModalButton=document.getElementById("modalButton");
 const closeModalButton=document.getElementsByClassName("closeModal");
+
 closeModalButton.item(0).addEventListener("click",myFunction);
-modalButton.addEventListener("click",myFunction);
+openModalButton.addEventListener("click",myFunction);
 
-// let modalContainer = document.getElementById("modal");
-console.log("modalButton",modalButton);
+console.log("modalButton",openModalButton);
+
 console.log("closeModalButton");
-
-function myFunction() {
+function myFunction(){
     let modalContainer = document.getElementById("modal");
-    if (modalContainer.style.display === "block") {
-        modalContainer.style.display = "none";
-    } else {
-        modalContainer.style.display = "block";
-    }
-  }
+    modalContainer.classList.toggle("hidden");
+}
+
+// function myFunction() {
+//     let modalContainer = document.getElementById("modal");
+//     if (modalContainer.style.display === "block") {
+//         modalContainer.style.display = "none";
+//     } else {
+//         modalContainer.style.display = "block";
+//     }
+//   }
