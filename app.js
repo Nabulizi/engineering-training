9d2d06a49b8ba3af42412f99b71246a1a065edfa
Nurali Abliz <nurali@Nuralis-MacBook-Pro.local>
2022-07-14 22:11:06 -0400

    DIG-72440 render static files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router=express.Router();    
-// const port = 3000;
+const port = 3000;
 
 router.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname+'/index.html'));
@@ -13,5 +13,11 @@ router.get('/',(req,res)=>{
 // })
 
 app.use('/',router);
+console.log(__dirname);
+app.use('/public',express.static(__dirname+'/public'))
 
 app.listen(process.env.port || 3000);
+
+app.listen(()=>{
+  console.log("Listening at port "+ port)
+})
